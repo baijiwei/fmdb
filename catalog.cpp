783c36645d23ea011768da501acb4e24c0037d08
baijiwei <wdbjw@163.com>
2017-12-24 18:29:50 +0800

    update flush

--- catalog.cpp
+++ catalog.cpp
@@ -1,8 +1,17 @@
 #include "catalog.h"
+#include <stdlib.h>
+#include <string.h>
+#include<vector>
 
 using namespace std;
 void fmdbCollection::FlushKVString()
 {
+	 std::vector<std::string> keys;
+	 for (std::map<string,string>::iterator it=kv_map.begin(); it!=kv_map.end(); ++it) {
+         keys.push_back(it->first);
+	 }
+
+     
      return;    
 }
 
@@ -12,6 +21,7 @@ void fmdbCollection::AddOneKVValue(string key, string value)
 	if (kv_map.end() != it) {
 		return;
 	}
+	curr_str_size += (key.size()+value.size()+2);
 	if (curr_str_size >= total_str_size) {
 		FlushKVString();
 	}
